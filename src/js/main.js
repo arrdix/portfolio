f4e553afd23364c41e95c8aa03309ad0bb987759
yudistiraardi <mail.ardix@gmail.com>
2023-12-10 22:26:17 +0700

    Implement smooth scrolling for navigation links

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,10 @@
-// Import our custom CSS
 import '../scss/index.scss';
 
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+import { SmoothScroll } from './features/smoothScroll';
 
-const disposables = document.querySelectorAll('.disposable');
-const chevron = document.querySelector('.bi-chevron-down');
-const tes1 = document.querySelectorAll('.tes1');
-const tes2 = document.querySelectorAll('.tes2');
-chevron.addEventListener('click', () => {
-  disposables.forEach((disposable) => {
-    disposable.classList.toggle('active');
-  });
-});
+document.addEventListener('DOMContentLoaded', initPages);
+
+function initPages() {
+  SmoothScroll.init();
+}
