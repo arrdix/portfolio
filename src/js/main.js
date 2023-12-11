4fdcf48684816a153d36d17dc041b376f9f445e1
yudistiraardi <mail.ardix@gmail.com>
2023-12-11 14:17:05 +0700

    Remove unused smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import '../scss/index.scss';
 
 import * as bootstrap from 'bootstrap';
-import {SmoothScroll} from './features/smoothScroll';
 import {DynamicHeight} from './features/DynamicHeight';
 import {DisposableHeader} from './features/DisposableHeader';
 import {DisposableContent} from './features/DisposableContents';
@@ -9,7 +8,6 @@ import {DisposableContent} from './features/DisposableContents';
 document.addEventListener('DOMContentLoaded', initPages);
 
 function initPages() {
-  // SmoothScroll.init();
   DynamicHeight.init();
   DisposableHeader.init();
   DisposableContent.init();
